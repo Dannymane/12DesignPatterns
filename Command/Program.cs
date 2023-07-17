d4dcfa531de2667b5f64c5926cdac6ade74345b6
Daniel <102802688+Dannymane@users.noreply.github.com>
2023-07-17 18:30:58 +0200

    Add files via upload

---
+++ Command/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Command
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Math.Sqrt(Double.Parse(args[0]));
+
+            using FileStream fs = File.OpenWrite(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wynik.txt"));
+            byte[] bytes = Encoding.UTF8.GetBytes(result.ToString());
+            fs.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
